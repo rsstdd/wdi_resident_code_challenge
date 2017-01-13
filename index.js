ee95c4d3f0fd1c1e39d515b9e1ddce3c65c666fd
rsstdd <rosstod@gmail.com>
2017-01-12 20:41:03 -0800

    Add more test cases

--- index.js
+++ index.js
@@ -18,17 +18,16 @@
         alert('File is not readable');
         break;
       case e.target.error.ABORT_ERR:
-        break; // noop
+        break;
       default:
         alert('An error occurred reading this file.');
     };
   }
 
   function updateProgress(e) {
-    // e is an ProgressEvent.
     if (e.lengthComputable) {
       var percentLoaded = Math.round((e.loaded / e.total) * 100);
-      // Increase the progress bar length.
+
       if (percentLoaded < 100) {
         progress.style.width = percentLoaded + '%';
         progress.textContent = percentLoaded + '%';
@@ -40,12 +39,9 @@
     progress.style.width = '0%';
     progress.textContent = '0%';
 
-    var files = e.target.files; // FileList object
-    console.log(files);
-    console.log('hello');
-    // Loop through the FileList and render image files as thumbnails.
-    for (var i = 0, f; f = files[i]; i++) {
+    var files = e.target.files;
 
+    for (var i = 0, f; f = files[i]; i++) {
 
       var reader = new FileReader();
 
@@ -54,34 +50,30 @@
       reader.onabort = function(e) {
         alert('File read cancelled');
       };
+
       reader.onloadstart = function(e) {
         document.getElementById('progress_bar').className = 'loading';
       };
-      reader.onload = function(e) {
-        // Ensure that the progress bar displays 100% at the end.
-        progress.style.width = '100%';
-        progress.textContent = '100%';
-        setTimeout("document.getElementById('progress_bar').className='';", 2000);
-      }
 
-      // Closure to capture the file information.
-      // Closure to capture the file information.
       reader.onload = (function (theFile) {
         return function (e) {
+          progress.style.width = '100%';
+          progress.textContent = '100%';
+          setTimeout("document.getElementById('progress_bar').className='';", 1000);
         try {
           json = JSON.parse(e.target.result);
-          console.log(e.target.result);
+          if (json.length === 0) {
+            alert('Add content to JSON file');
+          }
+          console.log(json);
         } catch (err) {
           alert('ex when trying to parse json = ' + err);
         }
       }
-      })(f);
+    })(f);
       reader.readAsText(f);
     }
   }
 
-  setTimeout("document.getElementById('progress_bar').className='';", 2000);
-
   document.getElementById('files').addEventListener('change', handleFileSelect, false);
-
 })();
